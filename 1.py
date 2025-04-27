b0beb13cd115a392b383feb1d80ff555a248360f
tsars <tsarskiytsarskiy@mail.ru>
2025-04-27 23:11:08 +0700

    1

--- 1.py
+++ 1.py
@@ -3,15 +3,15 @@ from PIL import Image
 # Список для хранения кадров.
 frames = []
 
-for frame_number in range(1, 6):
+for frame_number in range(1, 9):
     # Открываем изображение каждого кадра.
-    frame = Image.open(f'telegram/{frame_number}.png')
+    frame = Image.open(f'django/{frame_number}.png')
     # Добавляем кадр в список с кадрами.
     frames.append(frame)
 
 # Берем первый кадр и в него добавляем оставшееся кадры.
 frames[0].save(
-    'telegram.gif',
+    'django.gif',
     save_all=True,
     append_images=frames[1:],  # Срез который игнорирует первый кадр.
     optimize=True,
